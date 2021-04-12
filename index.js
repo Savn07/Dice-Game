d179717079370212e43f99e8f914b58cba9f05a0
Savn07 <56100528+Savn07@users.noreply.github.com>
2021-04-12 20:39:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var randomnumber1 = Math.floor(Math.random() * 6)+1;
+
+var randomdice1 = "dice"+randomnumber1+".png";
+
+var randomsource1 = "images/"+randomdice1;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomsource1);
+
+/* **************************************** */
+
+var randomnumber2 = Math.floor(Math.random() * 6)+1;
+
+var randomsource2 = "images/dice"+randomnumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",randomsource2);
+
+if(randomnumber1 > randomnumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 wins";
+}
+else if(randomnumber1 < randomnumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 2 wins";
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw";
+}
+
